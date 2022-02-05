3bb688092084eeb82d269435c8a0277e2a2e3432
Sumit Banik <sumitbanik.02@gmail.com>
2022-02-06 00:37:11 +0530

    [WIP] Testing Code Changes

--- main.go
+++ main.go
@@ -1,61 +1,54 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
+	// "bytes"
+	// "crypto/sha256"
+
 	"fmt"
+	"go-blockchain/handlers"
 )
 
-type Block struct {
-	Hash         []byte
-	Data         []byte
-	PreviousHash []byte
-}
+// func (c *Container) DeriveHash() {
+// 	info := bytes.Join([][]byte{c.Block.Data, c.Block.PreviousHash}, []byte{})
+// 	hash := sha256.Sum256(info)
+// 	c.Block.Hash = hash[:]
+// }
 
-type Blockchain struct {
-	blocks []*Block
-}
+// func zyx(data string, previousHash []byte)
 
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PreviousHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-}
+// func (c *Container) CreateBlock(data string, previousHash []byte) *gobc.Block {
+// 	block := &c.Block{[]byte{}, []byte(data), previousHash}
+// 	block.DeriveHash()
+// 	return block
+// }
 
-func CreateBlock(data string, previousHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), previousHash}
-	block.DeriveHash()
-	return block
-}
+// func (c *Container) AddBlock(data string) {
+// 	previousBlock := c.Blockchain.blocks[len(chain.blocks)-1]
+// 	newBlock := CreateBlock(data, previousBlock.Hash)
+// 	chain.blocks = append(chain.blocks, newBlock)
 
-func (chain *Blockchain) AddBlock(data string) {
-	previousBlock := chain.blocks[len(chain.blocks)-1]
-	newBlock := CreateBlock(data, previousBlock.Hash)
-	chain.blocks = append(chain.blocks, newBlock)
+// }
 
-}
+// func GenesisBlock() *Block {
+// 	return CreateBlock("Genesis", nil)
+// }
 
-func GenesisBlock() *Block {
-	return CreateBlock("Genesis", nil)
-}
-
-
-
-func InitBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{GenesisBlock()}}
-}
+// func InitBlockchain() *gobc.Blockchain {
+// 	return &Blockchain{[]*Block{GenesisBlock()}}
+// }
 
 func main() {
-	chain := InitBlockchain()
-	chain.AddBlock("This is a first block")
-	chain.AddBlock("This is a second block")
-	chain.AddBlock("This is a third block")
+	c, _ := handlers.NewContainer()
+	fmt.Printf("\nValue: %v",c)
+	// chain := InitBlockchain()
+	// chain.AddBlock("This is a first block")
+	// chain.AddBlock("This is a second block")
+	// chain.AddBlock("This is a third block")
 
-
-	for _ , block := range chain.blocks {
-		fmt.Printf("Prev Hash: %#x\n", block.PreviousHash)
-		fmt.Printf("Hash: %#x\n", block.Hash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Println()
-	}
+	// for _, block := range chain.blocks {
+	// 	fmt.Printf("Prev Hash: %#x\n", block.PreviousHash)
+	// 	fmt.Printf("Hash: %#x\n", block.Hash)
+	// 	fmt.Printf("Data: %s\n", block.Data)
+	// 	fmt.Println()
+	// }
 }
